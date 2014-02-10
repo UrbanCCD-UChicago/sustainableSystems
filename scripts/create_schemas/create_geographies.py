0d29abb9b38742a2d2e521a6c8314758a3bd674e
Hunter Owens <owens.hunter@gmail.com>
2014-02-10 14:20:18 -0600

    create geogpharies nad address tables

---
+++ scripts/create_schemas/create_geographies.py
@@ -0,0 +1,24 @@
+import psycopg2 
+from utils.db_connection import get_connection, close_connection
+
+def main():
+	conn = get_connection()
+	cur = conn.cursor()
+	statements = [
+		"""CREATE TABLE geographies(gid serial PRIMARY KEY NOT NULL,
+			geog geography(POLYGON,4326)
+			);""",
+		"""CREATE TABLE address_to_point(
+			gid serial PRIMARY KEY NOT NULL,
+			address text(255) NOT NULL, 
+			city text(255) NOT NULL,
+			zip integer NOT NULL
+			point geography(POINT,4326)
+			);"""]
+	for statement in statements:
+		cur.execute(statement)
+		conn.commit()
+	close_connection()
+	
+if __name__ == '__main__':
+	main()
